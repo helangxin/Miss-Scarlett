d8852738d65d2882c4c0d90bdbf8f9d38b5686ce
helangxin <helangxinhehe@163.com>
2016-04-25 22:47:27 +0800

    TarBar完成

--- 百思不得姐/百思不得姐/JDfriendTrendsController.m
+++ 百思不得姐/百思不得姐/JDfriendTrendsController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JDfriendTrendsController.h"
+#import "JDRecommeController.h"
 
 @interface JDfriendTrendsController ()
 
@@ -17,22 +18,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor redColor];
-    // Do any additional setup after loading the view.
+    
+    
+}
+- (IBAction)pushVC:(id)sender
+{
+    [self.navigationController pushViewController:[JDRecommeController new] animated:YES];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
---
+++ 百思不得姐/百思不得姐/class/friendTrends关注/Controller/JDRecommeController.h
@@ -0,0 +1,13 @@
+//
+//  JDRecommeController.h
+//  百思不得姐
+//
+//  Created by helangxin on 16/4/24.
+//  Copyright © 2016年 helangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDRecommeController : UIViewController
+
+@end
